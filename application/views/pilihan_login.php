4dcb5665bb758281ea73c5795be62b4809a7d345
mrizkiaditiya <62234016+mrizkiaditiya@users.noreply.github.com>
2020-06-17 16:22:46 +0700

    pilihan
    pilihan  login

---
+++ application/views/pilihan_login.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html style="" class=" js flexbox canvas canvastext webgl no-touch geolocation postmessage websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio localstorage sessionstorage webworkers applicationcache svg inlinesvg smil svgclippaths"><head>
+    <meta charset="utf-8"><style type="text/css">svg:not(:root).svg-inline--fa{overflow:visible}.svg-inline--fa{display:inline-block;font-size:inherit;height:1em;overflow:visible;vertical-align:-.125em}.svg-inline--fa.fa-lg{vertical-align:-.225em}.svg-inline--fa.fa-w-1{width:.0625em}.svg-inline--fa.fa-w-2{width:.125em}.svg-inline--fa.fa-w-3{width:.1875em}.svg-inline--fa.fa-w-4{width:.25em}.svg-inline--fa.fa-w-5{width:.3125em}.svg-inline--fa.fa-w-6{width:.375em}.svg-inline--fa.fa-w-7{width:.4375em}.svg-inline--fa.fa-w-8{width:.5em}.svg-inline--fa.fa-w-9{width:.5625em}.svg-inline--fa.fa-w-10{width:.625em}.svg-inline--fa.fa-w-11{width:.6875em}.svg-inline--fa.fa-w-12{width:.75em}.svg-inline--fa.fa-w-13{width:.8125em}.svg-inline--fa.fa-w-14{width:.875em}.svg-inline--fa.fa-w-15{width:.9375em}.svg-inline--fa.fa-w-16{width:1em}.svg-inline--fa.fa-w-17{width:1.0625em}.svg-inline--fa.fa-w-18{width:1.125em}.svg-inline--fa.fa-w-19{width:1.1875em}.svg-inline--fa.fa-w-20{width:1.25em}.svg-inline--fa.fa-pull-left{margin-right:.3em;width:auto}.svg-inline--fa.fa-pull-right{margin-left:.3em;width:auto}.svg-inline--fa.fa-border{height:1.5em}.svg-inline--fa.fa-li{width:2em}.svg-inline--fa.fa-fw{width:1.25em}.fa-layers svg.svg-inline--fa{bottom:0;left:0;margin:auto;position:absolute;right:0;top:0}.fa-layers{display:inline-block;height:1em;position:relative;text-align:center;vertical-align:-.125em;width:1em}.fa-layers svg.svg-inline--fa{-webkit-transform-origin:center center;transform-origin:center center}.fa-layers-counter,.fa-layers-text{display:inline-block;position:absolute;text-align:center}.fa-layers-text{left:50%;top:50%;-webkit-transform:translate(-50%,-50%);transform:translate(-50%,-50%);-webkit-transform-origin:center center;transform-origin:center center}.fa-layers-counter{background-color:#ff253a;border-radius:1em;-webkit-box-sizing:border-box;box-sizing:border-box;color:#fff;height:1.5em;line-height:1;max-width:5em;min-width:1.5em;overflow:hidden;padding:.25em;right:0;text-overflow:ellipsis;top:0;-webkit-transform:scale(.25);transform:scale(.25);-webkit-transform-origin:top right;transform-origin:top right}.fa-layers-bottom-right{bottom:0;right:0;top:auto;-webkit-transform:scale(.25);transform:scale(.25);-webkit-transform-origin:bottom right;transform-origin:bottom right}.fa-layers-bottom-left{bottom:0;left:0;right:auto;top:auto;-webkit-transform:scale(.25);transform:scale(.25);-webkit-transform-origin:bottom left;transform-origin:bottom left}.fa-layers-top-right{right:0;top:0;-webkit-transform:scale(.25);transform:scale(.25);-webkit-transform-origin:top right;transform-origin:top right}.fa-layers-top-left{left:0;right:auto;top:0;-webkit-transform:scale(.25);transform:scale(.25);-webkit-transform-origin:top left;transform-origin:top left}.fa-lg{font-size:1.33333em;line-height:.75em;vertical-align:-.0667em}.fa-xs{font-size:.75em}.fa-sm{font-size:.875em}.fa-1x{font-size:1em}.fa-2x{font-size:2em}.fa-3x{font-size:3em}.fa-4x{font-size:4em}.fa-5x{font-size:5em}.fa-6x{font-size:6em}.fa-7x{font-size:7em}.fa-8x{font-size:8em}.fa-9x{font-size:9em}.fa-10x{font-size:10em}.fa-fw{text-align:center;width:1.25em}.fa-ul{list-style-type:none;margin-left:2.5em;padding-left:0}.fa-ul>li{position:relative}.fa-li{left:-2em;position:absolute;text-align:center;width:2em;line-height:inherit}.fa-border{border:solid .08em #eee;border-radius:.1em;padding:.2em .25em .15em}.fa-pull-left{float:left}.fa-pull-right{float:right}.fa.fa-pull-left,.fab.fa-pull-left,.fal.fa-pull-left,.far.fa-pull-left,.fas.fa-pull-left{margin-right:.3em}.fa.fa-pull-right,.fab.fa-pull-right,.fal.fa-pull-right,.far.fa-pull-right,.fas.fa-pull-right{margin-left:.3em}.fa-spin{-webkit-animation:fa-spin 2s infinite linear;animation:fa-spin 2s infinite linear}.fa-pulse{-webkit-animation:fa-spin 1s infinite steps(8);animation:fa-spin 1s infinite steps(8)}@-webkit-keyframes fa-spin{0%{-webkit-transform:rotate(0);transform:rotate(0)}100%{-webkit-transform:rotate(360deg);transform:rotate(360deg)}}@keyframes fa-spin{0%{-webkit-transform:rotate(0);transform:rotate(0)}100%{-webkit-transform:rotate(360deg);transform:rotate(360deg)}}.fa-rotate-90{-webkit-transform:rotate(90deg);transform:rotate(90deg)}.fa-rotate-180{-webkit-transform:rotate(180deg);transform:rotate(180deg)}.fa-rotate-270{-webkit-transform:rotate(270deg);transform:rotate(270deg)}.fa-flip-horizontal{-webkit-transform:scale(-1,1);transform:scale(-1,1)}.fa-flip-vertical{-webkit-transform:scale(1,-1);transform:scale(1,-1)}.fa-flip-horizontal.fa-flip-vertical{-webkit-transform:scale(-1,-1);transform:scale(-1,-1)}:root .fa-flip-horizontal,:root .fa-flip-vertical,:root .fa-rotate-180,:root .fa-rotate-270,:root .fa-rotate-90{-webkit-filter:none;filter:none}.fa-stack{display:inline-block;height:2em;position:relative;width:2em}.fa-stack-1x,.fa-stack-2x{bottom:0;left:0;margin:auto;position:absolute;right:0;top:0}.svg-inline--fa.fa-stack-1x{height:1em;width:1em}.svg-inline--fa.fa-stack-2x{height:2em;width:2em}.fa-inverse{color:#fff}.sr-only{border:0;clip:rect(0,0,0,0);height:1px;margin:-1px;overflow:hidden;padding:0;position:absolute;width:1px}.sr-only-focusable:active,.sr-only-focusable:focus{clip:auto;height:auto;margin:0;overflow:visible;position:static;width:auto}</style><style data-styles="">ion-icon{visibility:hidden}.hydrated{visibility:inherit}</style>
+
+    <link rel="shortcut icon" href="https://musiq.id/assets/frontend/img/logo/icon.png">
+
+    <link rel="stylesheet" type="text/css" href="https://musiq.id/assets/frontend/plugins/slick/slick.css"><link rel="stylesheet" type="text/css" href="https://musiq.id/assets/frontend/plugins/slick/slick-theme.css"><link rel="stylesheet" type="text/css" href="https://musiq.id/assets/frontend/plugins/bootstrap/css/bootstrap.min.css"><link rel="stylesheet" type="text/css" href="https://musiq.id/assets/frontend/css/custom.css">
+    
+
+    <script src="https://www.googletagservices.com/activeview/js/current/osd.js?cb=%2Fr20100101"></script><script src="https://pagead2.googlesyndication.com/pagead/js/r20200609/r20190131/show_ads_impl_fy2019.js" id="google_shimpl"></script><script type="text/javascript" async="" src="https://www.google-analytics.com/analytics.js"></script><script defer="" src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+    <script src="https://unpkg.com/ionicons@4.5.5/dist/ionicons.js"></script><script src="https://unpkg.com/ionicons@4.5.5/dist/ionicons/ionicons.dkb1z4hj.js" type="module" crossorigin="true" data-resources-url="https://unpkg.com/ionicons@4.5.5/dist/ionicons/" data-namespace="ionicons"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.7.1/modernizr.min.js" type="text/javascript"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/cferdinandi/smooth-scroll/dist/smooth-scroll.polyfills.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.0/axios.min.js"></script>
+    
+    <script async="" src="https://www.googletagmanager.com/gtag/js?id=UA-143562280-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-143562280-1');
+    </script>
+    <script async="" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script>
+      (adsbygoogle = window.adsbygoogle || []).push({
+        google_ad_client: "ca-pub-8663748978015318",
+        enable_page_level_ads: true
+      });
+    </script>
+
+    <script type="text/javascript" src="https://musiq.id/assets/frontend/plugins/bootstrap/js/bootstrap.min.js"></script><script type="text/javascript" src="https://musiq.id/assets/frontend/plugins/slick/slick.min.js"></script><script type="text/javascript" src="https://musiq.id/assets/frontend/js/global.js"></script>    
+
+    <style type="text/css">
+        @media (max-width: 1366px ){
+            .container{
+                /*width: 1170px !important;*/
+            }
+        }
+
+        .slick-slide > .mentors > .col-sm-12{
+            background: #6c757d;
+            opacity: 0.9;
+        }
+        .slick-active > .mentors > .col-sm-12{
+            background: #6c757d !important;
+            opacity: 0.9;
+        }
+        .slick-center > .mentors > .col-sm-12{
+            background: #fff !important;
+            opacity: 1;
+        }
+        .slick-prev:before,
+        .slick-next:before {
+          color: #FFF;
+      }
+      .slick-prev, .slick-next{
+        margin-top:15px;height: 310px;width: 120px;z-index: 99;background: black;
+    }
+    .slick-prev, .slick-prev:hover, .slick-prev:active{
+        background: transparent;
+        background: -webkit-linear-gradient(to left, transparent, #1ac1c1);
+        background: linear-gradient(to left, transparent, #1ac1c1);
+
+    }
+    .slick-next,.slick-next:hover, .slick-next:active{
+        background: transparent;
+        background: -webkit-linear-gradient(to right, transparent, #1ac1c1);
+        background: linear-gradient(to right, transparent, #1ac1c1);
+
+    }
+</style>
+
+<script type="text/javascript">
+    var APIURL = 'https://apimusiq.id/';
+    var ASSETURL = 'https://musiq.id/assets/frontend/';
+    var BASEURL = 'https://musiq.id/';
+    var MEMBERSHIP = 'https://musiq.id/membership/'
+</script>
+
+<link rel="preload" href="https://adservice.google.co.id/adsid/integrator.js?domain=musiq.id" as="script"><script type="text/javascript" src="https://adservice.google.co.id/adsid/integrator.js?domain=musiq.id"></script><link rel="preload" href="https://adservice.google.com/adsid/integrator.js?domain=musiq.id" as="script"><script type="text/javascript" src="https://adservice.google.com/adsid/integrator.js?domain=musiq.id"></script><template><style>:host{display:inline-block;width:1em;height:1em;contain:strict;-webkit-box-sizing:content-box!important;box-sizing:content-box!important}.icon-inner,svg{display:block;fill:currentColor;stroke:currentColor;height:100%;width:100%}:host(.flip-rtl) .icon-inner{-webkit-transform:scaleX(-1);transform:scaleX(-1)}:host(.icon-small){font-size:18px!important}:host(.icon-large){font-size:32px!important}:host(.ion-color){color:var(--ion-color-base)!important}:host(.ion-color-primary){--ion-color-base:var(--ion-color-primary,#3880ff)}:host(.ion-color-secondary){--ion-color-base:var(--ion-color-secondary,#0cd1e8)}:host(.ion-color-tertiary){--ion-color-base:var(--ion-color-tertiary,#f4a942)}:host(.ion-color-success){--ion-color-base:var(--ion-color-success,#10dc60)}:host(.ion-color-warning){--ion-color-base:var(--ion-color-warning,#ffce00)}:host(.ion-color-danger){--ion-color-base:var(--ion-color-danger,#f14141)}:host(.ion-color-light){--ion-color-base:var(--ion-color-light,#f4f5f8)}:host(.ion-color-medium){--ion-color-base:var(--ion-color-medium,#989aa2)}:host(.ion-color-dark){--ion-color-base:var(--ion-color-dark,#222428)}</style></template></head>
+<body cz-shortcut-listen="true">
+
+
+
+<ins class="adsbygoogle adsbygoogle-noablate" data-adsbygoogle-status="done" style="display: none !important;"><ins id="aswift_0_expand" style="display:inline-table;border:none;height:0px;margin:0;padding:0;position:relative;visibility:visible;width:0px;background-color:transparent;"><ins id="aswift_0_anchor" style="display:block;border:none;height:0px;margin:0;padding:0;position:relative;visibility:visible;width:0px;background-color:transparent;"><iframe id="aswift_0" name="aswift_0" style="left:0;position:absolute;top:0;border:0;width:undefinedpx;height:undefinedpx;" sandbox="allow-forms allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts allow-top-navigation-by-user-activation" frameborder="0" src="https://googleads.g.doubleclick.net/pagead/ads?client=ca-pub-8663748978015318&amp;output=html&amp;adk=1812271804&amp;adf=3025194257&amp;lmt=1592143691&amp;plat=1%3A32776%2C2%3A16809992%2C8%3A32768%2C9%3A32776%2C10%3A32%2C11%3A32%2C16%3A8388608%2C17%3A32%2C24%3A32%2C25%3A32%2C30%3A1081344%2C32%3A32%2C40%3A32&amp;guci=2.2.0.0.2.2.0.0&amp;format=0x0&amp;url=https%3A%2F%2Fmusiq.id%2F&amp;ea=0&amp;flash=0&amp;pra=5&amp;wgl=1&amp;dt=1592143690703&amp;bpp=19&amp;bdt=2382&amp;idt=403&amp;shv=r20200609&amp;cbv=r20190131&amp;ptt=9&amp;saldr=aa&amp;abxe=1&amp;nras=1&amp;correlator=2871636130961&amp;frm=20&amp;pv=2&amp;ga_vid=344146253.1592143681&amp;ga_sid=1592143691&amp;ga_hid=1669056566&amp;ga_fc=0&amp;iag=0&amp;icsg=137438956538&amp;dssz=26&amp;mdo=0&amp;mso=0&amp;u_tz=420&amp;u_his=5&amp;u_java=0&amp;u_h=768&amp;u_w=1366&amp;u_ah=768&amp;u_aw=1304&amp;u_cd=24&amp;u_nplug=3&amp;u_nmime=4&amp;adx=-12245933&amp;ady=-12245933&amp;biw=1287&amp;bih=648&amp;scr_x=0&amp;scr_y=0&amp;eid=21065531&amp;oid=2&amp;pvsid=1970098489783478&amp;pem=576&amp;ref=https%3A%2F%2Fwww.google.com%2F&amp;rx=0&amp;eae=2&amp;fc=1920&amp;brdim=0%2C0%2C0%2C0%2C1304%2C0%2C1304%2C768%2C1304%2C665&amp;vis=1&amp;rsz=%7C%7Cs%7C&amp;abl=NS&amp;fu=9232&amp;bc=31&amp;ifi=0&amp;uci=a!0&amp;fsb=1&amp;dtd=520" marginwidth="0" marginheight="0" vspace="0" hspace="0" allowtransparency="true" scrolling="no" allowfullscreen="true" data-google-container-id="a!0" data-load-complete="true"></iframe></ins></ins></ins><iframe src="https://tpc.googlesyndication.com/sodar/sodar2/210/runner.html" width="0" height="0" style="display: none;"></iframe></body><iframe id="google_esf" name="google_esf" src="https://googleads.g.doubleclick.net/pagead/html/r20200609/r20190131/zrt_lookup.html#" data-ad-client="ca-pub-8663748978015318" style="display: none;"></iframe></html>
+<div class="text-center font-13 m-t-10 pad-30">
+	<h4>Masuk <span class="is-colored text-bold">Sebagai?</span>
+	</h4>
+</div>
+<div class="row m-t-30">
+	<div class="col-sm-3 m-t-10">
+		<div class="col-sm-12 bg-white is-bordered-radius removal-overflow ">
+			<div class="row">
+				<div class="col-md-9 pad-30"></div>
+					
+				<div class="col-md-3 no-pad d-none d-md-block d-lg-block d-xl-block">
+					<img src="https://musiq.id//assets/frontend/img/package/subs-beginner.png"></div>
+					
+					<div> 
+						<img src=""></div>
+						 <div class="text-center m-t-30 pad-20"><a href="#" data-toggle="modal" data-target="#registModal" class="btn btn-primary btn-sm " style="min-width: 130px;">
+                                    Mahasiswa&nbsp;<ion-icon name="paper-plane" role="img" class="hydrated" aria-label="paper plane"></ion-icon></a></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                     <div class="col-sm-3 m-t-10">
+                     	<div class="col-sm-12 bg-white is-bordered-radius removal-overflow ">
+                     		<div class="row">
+                     			<div class="col-md-9 pad-30">
+                     		</div>	
+                     	<div class="col-md-3 no-pad d-none d-md-block d-lg-block d-xl-block">
+						<img src="https://musiq.id//assets/frontend/img/package/subs-intermediate.png"></div>
+                     		<div>
+                     			<img src="">
+                     		</div>
+                     		<div class="row is-bordered-top is-bordered-grey">
+                     				<div class="text-center m-t-30 pad-20">
+                     					<a href="#" data-toggle="modal" data-target="#registModal" class="btn btn-info btn-sm " style="min-width: 130px;">
+                                    Panitia&nbsp;
+                                    <ion-icon name="paper-plane" role="img" class="hydrated" aria-label="paper plane">
+                                    	
+                                    </ion-icon>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> 
+             <div class="col-sm-3 m-t-10">
+             	<div class="col-sm-12 bg-white is-bordered-radius removal-overflow ">
+             		<div class="row">
+             			<div class="col-md-9 pad-30">
+             			</div> 
+             			<div class="col-md-3 no-pad d-none d-md-block d-lg-block d-xl-block">
+             				<img src="https://musiq.id//assets/frontend/img/package/subs-advanced.png">
+             			</div>
+             		</div> 
+             		<div class="row is-bordered-top is-bordered-grey">
+             				<div class="text-center m-t-30 pad-20">
+             					<a href="#" data-toggle="modal" data-target="#registModal" class="btn btn-warning btn-sm " style="min-width: 130px;"> 
+                                    Pembimbing&nbsp;<ion-icon name="paper-plane" role="img" class="hydrated" aria-label="paper plane"></ion-icon></a></div></div></div></div></div></div>
+</html>
