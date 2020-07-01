2acc47521d42b8ddde791f3b3b245a3f9402b38f
mrizkiaditiya <62234016+mrizkiaditiya@users.noreply.github.com>
2020-07-02 01:12:58 +0700

    Update diskusi_bimbingan.php
    nihhhh yg baru gan

--- application/views/pkl/mahasiswa/pages/diskusi_bimbingan.php
+++ application/views/pkl/mahasiswa/pages/diskusi_bimbingan.php
@@ -22,74 +22,35 @@
 
 
 </head>
+<html>
+<head>
+  
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+  <script type="text/javascript" src="js/jquery.js"></script>
+  <script type="text/javascript" src="js/bootstrap.js"></script>
+</head>
 <body>
-   <!-- Content Wrapper. Contains page content -->
-    <!-- Main content -->
-    <section class="content">
-
-      <!-- Default box -->
-      <div class="card">
-        <div class="card-header">
-          <div class="card-tools">
-            <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
-              <i class="fas fa-minus"></i></button>
-            <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
-              <i class="fas fa-times"></i></button>
-          </div>
-        </div>
-        <div class="card-body">
-        <!-- TABEL TAMBAH DATA DOSEN -->
- <div class="card-body">
-  <span id="success_message"></span>
-  <br>
-  <button id="add-button" type="button" class="btn btn-sm btn-primary"><i class="fas fa-plus-circle"></i> Tambah</button>
-  <br>
-  <br>
-
-  <div class="card" style="width: 40rem;">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="card-link">Balas</a>
-    <a href="#" class="card-link">Edit</a>
+ 
+  <div class="container">   
+   
+    <form class="form-horizontal">
+      <div class="form-group">
+        <label class="control-label col-sm-2" for="nama">Reply</label>
+        <div class="form-group">
+      <textarea type="text" class="form-control" id="alamat"></textarea>
+    </div>
+    <div class="container" style="margin-top:10px;">
+  <div class="row">
+            <div class="col-xs-12">
+                <form method="POST">
+                    <div class="form-group">
+                        <label for="InputFile">File input</label>
+                        <input type="file" id="InputFile" name="file">
+                    </div>
+      <button type="submit" class="btn btn-danger">Simpan</button>
+      <button type="submit" class="btn btn-danger">Cancel</button>
+    </form>   
   </div>
-</div>
-
-         <!-- MODAL TAMBAH DATA -->
-<div class="modal fade" id="modal-tambah">
-<div class="modal-dialog">
-  <div class="modal-content">
-  <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">Tambah</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body">
-            <form method="post" id="user_form">
-              <div class="form-group">
-                <input type="text" name="tanggal_sidang" id="tanggal_sidang" class="form-control" placeholder="judul" value="">
-              </div>
-              <div class="form-group">
-                <input type="text" name="dosen" id="dosen" class="form-control" placeholder="Isi" value="">
-              </div>
-              <div class="form-group">
-             
-
-              <div class="modal-footer">
-              <input type="hidden" name="user_id" id="user_id" />
-              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-              <input type="hidden" name="data_action" id="data_action" value="Insert" />
-              <input type="submit" name="action" id="action" class="btn btn-success" value="Add" />
-              </div>
-              </form>
-            </div>
-            </div>
-          </div>
-          </div>
-        <!-- TUTUP MODAL TAMBAH DATA -->
-
-</div>
-</div>
+ 
 </body>
 </html>
