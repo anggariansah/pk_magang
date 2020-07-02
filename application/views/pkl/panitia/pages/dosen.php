2fb94dc1f9ab9c285f929f0cb1aedb61fa1b5e16
Anggiawanprasetyo <56067686+Anggiawanprasetyo@users.noreply.github.com>
2020-07-02 13:44:09 +0700

    dataTables tapi blm muncul

--- application/views/pkl/panitia/pages/dosen.php
+++ application/views/pkl/panitia/pages/dosen.php
@@ -206,3 +206,12 @@
 
 
 </script>
+<!-- <script src="adminLTE/plugins/datatables/jquery.dataTables.js"></script> -->
+<script type="text/javascript" src="<? echo base_url() ?>assets/AdminLTE/plugins/datatables/jquery.dataTables.js"></script>
+<script type="text/javascript" src="<? echo base_url() ?>assets/AdminLTE/plugins/datatables-bs4/js/dataTables.bootstrap4.js"></script>
+<!-- <script src="adminLTE/plugins/datatables-bs4/js/dataTables.bootstrap4.js"></script> -->
+<script>
+  $(function () {
+    $("#example1").DataTable();
+  });
+  </script>
